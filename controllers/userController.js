c0a11e0b936b8c7e7f3e7ea668cfd779487fc985
arpitjaswal <iamarpitjaswal@gmail.com>
2023-08-30 19:02:18 +0530

    feat: created some more userControllers
    Signed-off-by: arpitjaswal <iamarpitjaswal@gmail.com>

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,11 @@
 import user from "../models/user";
+import user from "../models/user";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv"
+import nodemailer from "nodemailer";
+
+dotenv.config();
 
 //controller for creating an user
 const createUser = async (req,res)=>{
@@ -13,4 +19,56 @@ const createUser = async (req,res)=>{
         res.status(500).json({error:error.message});
     }
 }
+//login controller
+const login = async (req,res)=>{
+    try {
+        const {username, password} = req.body;
+        //try to find if there is an user with that username
+        const User = await user.findOne({username});
+        if(!User){
+            return res.status(401).json({ error: 'No such user present' });
+        }
+       
+        const matchPassword = bcrypt.compare(password, User.password);
+        if(!passwordMatch){
+            return res.status(401).json({ error: 'Invalid password' });
+        }
+
+        const jwtToken = jwt.sign({userID: user._id}, process.env.SECRETKEY, {expiresIn:"1h"});
+        res.json({jwtToken});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+
+//forgot password
+const transporter = nodemailer.createTransport({
+    service:"Gmail",
+    auth:{
+        user: process.env.email,
+        password: process.env.password,
+    },
+})
+
+
+const forgotPassword = async(req,res)=>{
+    try {
+        const {email} = req.body;
+
+        const mailOptions = {
+            from: process.env.email,
+            to: email,
+            subject: "Reset Password",
+            text:"Click the link to reset your password:",
+        }
+        await transporter.sendMail(mailOptions);
+        res.json({ message: 'Password reset email sent' });
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+
 
